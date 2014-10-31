3c33a50f6ceaf845c391b44884df440ade825b1e
Malinda Kumarasinghe <malindakpt@gmail.com>
2014-10-31 12:54:27 +0530

    Implemented inherited methods @ engines

--- org.cse.visiri.engine/src/main/java/org/cse/visiri/engine/SiddhiEngine.java
+++ org.cse.visiri.engine/src/main/java/org/cse/visiri/engine/SiddhiEngine.java
@@ -1,8 +1,29 @@
 package org.cse.visiri.engine;
 
+import org.cse.visiri.util.Event;
+
 /**
  * Created by Malinda Kumarasinghe on 10/31/2014.
  */
-public class SiddhiEngine extends  CEPEngine{
+public class SiddhiEngine extends CEPEngine{
 
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Object saveState() {
+        return null;
+    }
+
+    @Override
+    public void sendEvent(Event e) {
+
+    }
 }
