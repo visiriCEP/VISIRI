6fed03eb961fa6929a9afe476a3c66001305a6ac
ujitha <dgujitha@gmail.com>
2014-10-31 12:10:43 +0530

    create QueryDistribution class

--- org.cse.visiri.algo/src/main/java/QueryDistribution.java
+++ org.cse.visiri.algo/src/main/java/QueryDistribution.java
@@ -1,5 +1,35 @@
+import org.cse.visiri.util.Query;
+
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by visiri on 10/31/14.
  */
 public class QueryDistribution {
+
+    private HashMap<Query,String> queryToNodeMap;
+    private HashMap<Query,List<Query>> originalToGeneratedQuery;
+
+    public QueryDistribution(){
+        this.queryToNodeMap=new HashMap<Query, String>();
+        this.originalToGeneratedQuery=new HashMap<Query, List<Query>>();
+    }
+
+    public void addQueryToNode(Query query,String nodeIp){
+        this.getQueryToNodeMap().put(query, nodeIp);
+    }
+
+    public void addGeneratedQueryToOriginal(Query query,List<Query> queryList){
+        this.getOriginalToGeneratedQuery().put(query, queryList);
+    }
+
+
+    public HashMap<Query, String> getQueryToNodeMap() {
+        return queryToNodeMap;
+    }
+
+    public HashMap<Query, List<Query>> getOriginalToGeneratedQuery() {
+        return originalToGeneratedQuery;
+    }
 }
