a76b20c119db654a45a5a05d7f172cc7e45860f8
GeethTharanga <getthh@gmail.com>
2014-10-31 10:34:53 +0530

    Added Environment

---
+++ org.cse.visiri.communications.environment/src/main/java/org/cse/visiri/communications/Environment.java
@@ -0,0 +1,16 @@
+package main.java.org.cse.visiri.communications;
+
+/**
+ * Created by Geeth on 2014-10-31.
+ */
+public class Environment {
+
+
+
+
+
+    String getNodeId()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
