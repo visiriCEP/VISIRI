e61f0e601e2289b6c9a4b7570350c61cbb9be60d
ujitha <dgujitha@gmail.com>
2014-10-31 10:22:43 +0530

    create Query.java and Utilization.java and UtilizationUpdater.java

---
+++ org.cse.visiri.util/src/main/java/org/cse/visiri/util/UtilizationUpdater.java
@@ -0,0 +1,21 @@
+package org.cse.visiri.util;
+
+/**
+ * Created by visiri on 10/31/14.
+ */
+public class UtilizationUpdater {
+
+
+    public UtilizationUpdater(){
+
+    }
+
+    public void start(){
+
+    }
+
+    public void stop(){
+        
+    }
+
+}
