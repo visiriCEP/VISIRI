ea4dd62bb86f61ff53bc5d6bf2e1279289d71412
ujitha <dgujitha@gmail.com>
2014-11-03 21:19:29 +0530

    change streamDefinition array to List

---
+++ org.cse.visiri.engine/src/main/java/org/cse/visiri/engine/DirectPassEngine.java
@@ -0,0 +1,28 @@
+package org.cse.visiri.engine;
+
+import org.cse.visiri.util.Event;
+
+/**
+ * Created by Malinda Kumarasinghe on 10/31/2014.
+ */
+public class DirectPassEngine extends CEPEngine {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Object saveState() {
+        return null;
+    }
+
+    @Override
+    public void sendEvent(Event e) {
+
+    }
+}
--- org.cse.visiri.engine/src/main/java/org/cse/visiri/engine/SiddhiEngine.java
+++
@@ -1,8 +0,0 @@
-package org.cse.visiri.engine;
-
-/**
- * Created by Malinda Kumarasinghe on 10/31/2014.
- */
-public class SiddhiEngine extends  CEPEngine{
-
-}
