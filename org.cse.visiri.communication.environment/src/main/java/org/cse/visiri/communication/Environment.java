411b546702c627378393e7262b2467c301c4ac4a
Malinda Kumarasinghe <malindakpt@gmail.com>
2014-11-04 18:11:39 +0530

    Created Methods

--- org.cse.visiri.communication.environment/src/main/java/org/cse/visiri/communication/Environment.java
+++ org.cse.visiri.communication.environment/src/main/java/org/cse/visiri/communication/Environment.java
@@ -5,6 +5,7 @@ import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.Member;
 import org.cse.visiri.util.Query;
+import org.cse.visiri.util.QueryDistribution;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,29 +17,76 @@ import java.util.Set;
  */
 public class Environment {
 
-    private static Environment environment;
+    private final String UTILIZATION_MAP="UTILIZATION_MAP";
+    private final String NODE_QUERY_MAP="NODE_QUERY_MAP";
+    private final String ORIGINAL_TO_DEPLOYED_MAP="ORIGINAL_TO_DEPLOYED_MAP";
+    private final String SUBSCRIBER_MAP="SUBSCRIBER_MAP";
+
+    private List<String> bufferingEventList;
     private EnvironmentChangedCallback changedCallback;
-    private Map<String,Boolean> nodeUpdatedLatestChanges;
-    private HazelcastInstance hzInstance;
+
+   // private Map<String,Boolean> nodeUpdatedLatestChanges;
+   // private Map<Query,List<Query>> originalQueryToDeployedQueryMap;
+   // private Map<String,List<Query>> nodeToQueriesMap;
+    //private Map<String, Double> utilizationMap;
+    //private Map<String,List<String>> subscriberMapping;
 
 
+    private HazelcastInstance hzInstance = null;
+    private static Environment instance = null;
 
-    //private Environment(EnvironmentChangedCallback callback)
-    private Environment() {                                                //To be modified
-        // changedCallback = callback;
+
+    private Environment() {
         Config cfg = new Config();
         hzInstance = Hazelcast.newHazelcastInstance(cfg);
+        bufferingEventList=new ArrayList<String>();
+
+//        utilizationMap=hzInstance.getMap(UTILIZATION_MAP);
+//        nodeToQueriesMap=hzInstance.getMap(NODE_QUERY_MAP);
+//        originalQueryToDeployedQueryMap=hzInstance.getMap(ORIGINAL_TO_DEPLOYED_MAP);
+//        subscriberMapping=hzInstance.getMap(SUBSCRIBER_MAP);
     }
     
  
 
     public void setChangedCallback(EnvironmentChangedCallback callback)
     {
-        changedCallback = callback;
+        this.changedCallback = callback;
     }
 
-    private static Environment instance = null;
+    public EnvironmentChangedCallback getChangedCallback(){
+        return changedCallback;
+    }
 
+
+    public void setNodeUtilization(String nodeIp,Double value){
+        hzInstance.getMap(UTILIZATION_MAP).put(nodeIp, value);
+    }
+
+    public void addQueryDistribution(QueryDistribution queryDistribution){
+
+       //Adding to originalQueryToDeployedQueryMap
+
+        Map<Query,List<Query>> generatedQueries=queryDistribution.getGeneratedQueries();
+
+        for(Query query : generatedQueries.keySet()){
+            hzInstance.getMap(ORIGINAL_TO_DEPLOYED_MAP).put(query,generatedQueries.get(query));
+        }
+
+        //Adding to nodeToQueriesMap
+        Map<Query,String> queryAllocation=queryDistribution.getQueryAllocation();
+
+        for(Query query : queryAllocation.keySet()){
+            String ip=queryAllocation.get(query);
+            List<Query> queryList= (List<Query>)(hzInstance.getMap(NODE_QUERY_MAP).get(ip));//.get(ip);
+
+            if(queryList==null){
+                queryList=new ArrayList<Query>();
+            }
+            queryList.add(query);
+            hzInstance.getMap(NODE_QUERY_MAP).put(ip, queryList);
+        }
+    }
 
     /** Singleton Accessor **/
     public static Environment getInstance()
@@ -62,7 +110,7 @@ public class Environment {
 
     public Map<Query,List<Query>> getOriginalToDeployedQueriesMapping()
     {
-        throw new UnsupportedOperationException();
+        return hzInstance.getMap(ORIGINAL_TO_DEPLOYED_MAP);
     }
 
 
@@ -70,37 +118,32 @@ public class Environment {
     {
         return hzInstance.getCluster().getLocalMember().getInetSocketAddress().toString();
     }
-    public Map<Query,List<Query>> getGeneratedQueryMapping()
+
+    public Map<String,List<Query>> getNodeQueryMap()
     {
-    
-        throw new UnsupportedOperationException();
-    }
-
-
-
-    public static void main(String args[]) {
-
-        //  System.out.println("My "+Environment.getInstance().getNodeId());
-    }
-    public Map<String,List<Query>> getNodeQueryTable()
-    {
-        throw new UnsupportedOperationException();
+        return hzInstance.getMap(NODE_QUERY_MAP);
     }
 
 
     public Map<String,Double> getNodeUtilizations()
     {
-        throw new UnsupportedOperationException();
+        return hzInstance.getMap(UTILIZATION_MAP);
     }
 
     public List<String> getBufferingEventList()
     {
-        throw new UnsupportedOperationException();
+        return bufferingEventList;
     }
 
     public Map<String,List<String>> getSubscriberMapping()
     {
-        throw new UnsupportedOperationException();
+        return hzInstance.getMap(SUBSCRIBER_MAP);
+    }
+
+
+    public static void main(String args[]) {
+        //Environment.getInstance().setNodeUtilization("1231",12.5);
+        //System.out.println("My "+Environment.getInstance().getNodeUtilizations().get("1231"));
     }
 
 }
