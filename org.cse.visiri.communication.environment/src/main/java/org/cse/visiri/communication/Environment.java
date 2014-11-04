e199f9c709227da786f26f988b346c4440023b8c
GeethTharanga <getthh@gmail.com>
2014-11-04 11:38:37 +0530

    Added environment class methods added algorithm methods

--- org.cse.visiri.communication.environment/src/main/java/org/cse/visiri/communication/Environment.java
+++ org.cse.visiri.communication.environment/src/main/java/org/cse/visiri/communication/Environment.java
@@ -12,21 +12,71 @@ public class Environment {
 
     EnvironmentChangedCallback changedCallback;
 
-    public Environment(EnvironmentChangedCallback callback)
-    {
-        changedCallback = callback;
-        throw new UnsupportedOperationException();
-    }
-
     public Map<Query,List<Query>> getOriginalToDeployedQueriesMapping()
     {
         throw new UnsupportedOperationException();
     }
 
+    public void setChangedCallback(EnvironmentChangedCallback callback)
+    {
+        changedCallback = callback;
+    }
+
+    private static Environment instance = null;
+
+    private Environment()
+    {
+
+    }
+
+    /** Singleton Accessor **/
+    public static Environment getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new Environment();
+        }
+        return instance;
+    }
 
     String getNodeId()
     {
-
         throw new UnsupportedOperationException();
     }
+
+    public List<String> getNodeIdList()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> getDispatcherIdList()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Map<Query,List<Query>> getGeneratedQueryMapping()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Map<String,List<Query>> getNodeQueryTable()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Map<String,Double> getNodeUtilizations()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<String> getBufferingEventList()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public Map<String,List<String>> getSubscriberMapping()
+    {
+        throw new UnsupportedOperationException();
+    }
+
 }
