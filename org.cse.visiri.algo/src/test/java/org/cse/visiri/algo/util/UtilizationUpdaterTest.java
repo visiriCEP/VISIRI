7c773e48e0709344b02831b3b2d1df0018e435b9
ujitha <dgujitha@gmail.com>
2014-11-22 10:37:45 +0530

    UtilizationUpdaterTest class is added

---
+++ org.cse.visiri.algo/src/test/java/org/cse/visiri/algo/util/UtilizationUpdaterTest.java
@@ -0,0 +1,22 @@
+package org.cse.visiri.algo.util;
+
+import junit.framework.TestCase;
+
+public class UtilizationUpdaterTest extends TestCase {
+
+    UtilizationUpdater utilizationUpdater;
+
+    public void testStart() throws Exception {
+        utilizationUpdater=new UtilizationUpdater();
+        utilizationUpdater.start();
+
+    }
+
+    public void testUpdate() throws Exception {
+
+    }
+
+    public void testStop() throws Exception {
+
+    }
+}
