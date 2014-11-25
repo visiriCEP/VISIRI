221234b0688953b1a7e1cb89ba4016251413301a
Malinda Kumarasinghe <malindakpt@gmail.com>
2014-11-25 11:40:44 +0530

    Created DynamicQueryDistribution class

---
+++ org.cse.visiri.util/src/main/java/org/cse/visiri/util/DynamicQueryDistribution.java
@@ -0,0 +1,21 @@
+package org.cse.visiri.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Malinda Kumarasinghe on 11/25/2014.
+ */
+public class DynamicQueryDistribution {
+
+    private Map<String,String> engineAllocation;//query->IP
+
+    public  DynamicQueryDistribution(){
+        engineAllocation=new HashMap<String, String>();
+    }
+
+    public Map<String, String> getGeneratedQueries() {
+        return engineAllocation;
+    }
+}
