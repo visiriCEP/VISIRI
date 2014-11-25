65483defbe00da88bb0cff09c8a9db66ab871e90
lasitha1991 <lasitha1991@hotmail.com>
2014-11-25 17:47:49 +0530

    dynamic query distribution: initial

---
+++ org.cse.visiri.algo/src/main/java/org/cse/visiri/algo/DynamicQueryDistributionAlgo.java
@@ -0,0 +1,18 @@
+package org.cse.visiri.algo;
+
+
+import org.cse.visiri.util.Query;
+import org.cse.visiri.util.QueryDistribution;
+
+import java.util.List;
+
+/**
+ * Created by lasitha on 11/25/14.
+ */
+public class DynamicQueryDistributionAlgo extends QueryDistributionAlgo{
+
+    @Override
+    public QueryDistribution getQueryDistribution(List<Query> queries) {
+        return null;
+    }
+}
