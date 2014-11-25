1e09a0af032c37a0453b900261f97404db7bd95b
ujitha <dgujitha@gmail.com>
2014-11-25 16:25:37 +0530

    added WindowQueue data structure

---
+++ org.cse.visiri.util/src/main/java/org/cse/visiri/util/WindowQueue.java
@@ -0,0 +1,36 @@
+package org.cse.visiri.util;
+
+/**
+ * Created by visiri on 11/25/14.
+ */
+public class WindowQueue {
+
+    private int size;
+    private double[] valueArray;
+    private int pos;
+
+    public WindowQueue(int size){
+
+        this.size=size;
+        valueArray=new double[size];
+        pos=0;
+    }
+
+    public void add(double val){
+        if(pos==size-1) {
+            pos = 0;
+        }
+        valueArray[pos]=val;
+        pos++;
+    }
+
+    public double getAverage(){
+        double sum=0;
+        for(int i=0;i<size;i++){
+            sum+=valueArray[i];
+        }
+
+        return sum/size;
+    }
+
+}
