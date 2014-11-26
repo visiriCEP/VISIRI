52ef2df0b55a6ad57c785ce660c9696f32573caa
ujitha <dgujitha@gmail.com>
2014-11-26 10:18:33 +0530

    Create Algofactory class

---
+++ org.cse.visiri.algo/src/main/java/org/cse/visiri/algo/AlgoFactory.java
@@ -0,0 +1,22 @@
+package org.cse.visiri.algo;
+
+/**
+ * Created by visiri on 11/26/14.
+ */
+public class AlgoFactory {
+
+    public AlgoFactory(){};
+
+    public static QueryDistributionAlgo createAlgorithm(int type){
+
+            if(type==QueryDistributionAlgo.SCTXPF_ALGO){
+                return new SCTXPFDistributionAlgo();
+            }else if(type==QueryDistributionAlgo.ROUNDROBIN_ALGO){
+                return new RoundRobinDistributionAlgo();
+            }else if(type==QueryDistributionAlgo.RANDOM_DISTRIBUTOR_ALGO){
+                return new RandomDistributionAlgo();
+            }
+
+        throw new UnsupportedOperationException();
+    }
+}
