7621c4dab9c41d52047734fbf5880365379b4a4d
ujitha <dgujitha@gmail.com>
2014-12-06 16:27:38 +0530

    added Evaluation class2

--- org.cse.visiri.app/src/main/java/org/cse/visiri/app/Evaluation.java
+++ org.cse.visiri.app/src/main/java/org/cse/visiri/app/Evaluation.java
@@ -1,5 +1,6 @@
 package org.cse.visiri.app;
 
+import org.cse.visiri.util.Configuration;
 import org.cse.visiri.util.Query;
 import org.cse.visiri.util.StreamDefinition;
 
@@ -16,9 +17,6 @@ public class Evaluation {
         StreamDefinition inputStreamDef=new StreamDefinition();
         inputStreamDef.setStreamId("players");
         List<StreamDefinition.Attribute> attributeList=new ArrayList<StreamDefinition.Attribute>();
-       // StreamDefinition.Attribute attribute=new StreamDefinition.Attribute();
-
-
 
         inputStreamDef.addAttribute("sid", StreamDefinition.Type.INTEGER);
         inputStreamDef.addAttribute("x", StreamDefinition.Type.INTEGER);
@@ -27,10 +25,28 @@ public class Evaluation {
         inputStreamDef.addAttribute("v", StreamDefinition.Type.INTEGER);
         inputStreamDef.addAttribute("a", StreamDefinition.Type.INTEGER);
 
-        StreamDefinition outputStreamdef=inputStreamDef;
-        List<StreamDefinition> streamDefinitionList=new ArrayList<StreamDefinition>();
-        streamDefinitionList.add(inputStreamDef);
-       // Query query1=new Query()
+        List<StreamDefinition> inputStreamDefinitionList=new ArrayList<StreamDefinition>();
+        inputStreamDefinitionList.add(inputStreamDef);
+
+        StreamDefinition outputStreamdef1=new StreamDefinition();
+        outputStreamdef1.setStreamId("stopplayer");
+        outputStreamdef1.addAttribute("sid", StreamDefinition.Type.INTEGER);
+        outputStreamdef1.addAttribute("x", StreamDefinition.Type.INTEGER);
+        outputStreamdef1.addAttribute("y", StreamDefinition.Type.INTEGER);
+        outputStreamdef1.addAttribute("z", StreamDefinition.Type.INTEGER);
+        outputStreamdef1.addAttribute("v", StreamDefinition.Type.INTEGER);
+        outputStreamdef1.addAttribute("a", StreamDefinition.Type.INTEGER);
+
+        String q1="from players[v<=1]" +
+                "select sid,x,y,z,v,a" +
+                "insert into stopplayer";
+        Query query1=new Query(q1,inputStreamDefinitionList,outputStreamdef1,"1", Configuration.ENGINE_TYPE_SIDDHI);
+
+
+
+
+
+
 
     }
 }
