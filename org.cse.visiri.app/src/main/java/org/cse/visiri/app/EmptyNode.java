b1c3e298ce40e72e3bc99dd763b375ddcded90de
GeethTharanga <getthh@gmail.com>
2014-12-07 12:08:46 +0530

    empty node

---
+++ org.cse.visiri.app/src/main/java/org/cse/visiri/app/EmptyNode.java
@@ -0,0 +1,27 @@
+package org.cse.visiri.app;
+
+import org.cse.visiri.communication.Environment;
+import org.cse.visiri.core.Node;
+
+import java.util.Scanner;
+
+/**
+ * Created by Geeth on 2014-12-07.
+ */
+public class EmptyNode {
+
+
+    public static void main(String[] args) throws Exception {
+        Environment environment;
+        Evaluation evaluation=new Evaluation();
+        Node node=new Node();
+
+        node.initialize();
+
+        Scanner sc = new Scanner(System.in);
+        node.start();
+
+        System.out.println("Started");
+        sc.next();
+    }
+}
