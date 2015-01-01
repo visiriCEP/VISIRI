b58e480087fb339e11dabdc484b353fe46ab44be
GeethTharanga <getthh@gmail.com>
2015-01-01 12:05:55 +0530

    dynamic-  fixes

--- org.cse.visiri.util/src/main/java/org/cse/visiri/util/costmodelcalc/CostModelCalculator.java
+++ org.cse.visiri.util/src/main/java/org/cse/visiri/util/costmodelcalc/CostModelCalculator.java
@@ -11,7 +11,7 @@ public class CostModelCalculator {
 
     public double calculateCost(final Query q){
         // consider every query has same cost for simplicity
-        System.out.println(q.getEngineId()+":"+q.getQuery());
+    //    System.out.println(q.getEngineId()+":"+q.getQuery());
         if(q.getEngineId()==0){//CEPEngine.ENGINE_TYPE_DIRECT
             return new DirectEngCostModelCalculator().calculateCost(q);
         }
