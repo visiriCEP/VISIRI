bfbd12d17c11ae69b42bd4e5d0dda63994f6d199
ujitha <dgujitha@gmail.com>
2015-01-13 23:17:49 +0530

    Added class TransferbleEngine

---
+++ org.cse.visiri.util/src/main/java/org/cse/visiri/util/costmodelcalc/TransferbleEngine.java
@@ -0,0 +1,33 @@
+package org.cse.visiri.util.costmodelcalc;
+
+import org.cse.visiri.util.Query;
+import org.cse.visiri.util.StreamDefinition;
+import org.cse.visiri.util.TransferbleQuery;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by visiri on 1/13/15.
+ */
+public class TransferbleEngine {
+    private Map<String,List<Query>> nodeTransferbleQueryMap;
+    private Set<StreamDefinition> completlyRemovedEvents;
+
+
+    public TransferbleEngine(Map<String,List<Query>> nodeTransferbleQueryMap,Set<StreamDefinition> completlyRemovedEvents){
+        this.nodeTransferbleQueryMap=nodeTransferbleQueryMap;
+        this.completlyRemovedEvents=completlyRemovedEvents;
+    }
+
+
+    public Map<String,List<Query>> getNodeTransferbleQueryMap(){
+        return nodeTransferbleQueryMap;
+    }
+
+    public Set<StreamDefinition> getCompletlyRemovedEvents(){
+        return completlyRemovedEvents;
+    }
+
+}
