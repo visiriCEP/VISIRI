564d960e6a51ea528b56bc192a3217a3357de10d
GeethTharanga <getthh@gmail.com>
2015-02-01 22:33:05 +0530

    modified stxpf

--- org.cse.visiri.algo/src/main/java/org/cse/visiri/algo/SCTXPFDistributionAlgo.java
+++ org.cse.visiri.algo/src/main/java/org/cse/visiri/algo/SCTXPFDistributionAlgo.java
@@ -19,7 +19,7 @@ and considers event types instead of used event attributes
 **/
 public class SCTXPFDistributionAlgo extends QueryDistributionAlgo {
 
-    public final double queryCountThreshold = 30;
+    public final double queryCountThreshold = 10;
 
     @Override
     public QueryDistribution getQueryDistribution(QueryDistributionParam param) {
