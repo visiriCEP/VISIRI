d48c3422e2e41f1ff3af85856b522c6958a5c41a
Malinda Kumarasinghe <malindakpt@gmail.com>
2015-02-04 17:28:02 +0530

    Added eventRate check

--- org.cse.visiri.core/src/main/java/org/cse/visiri/core/Agent.java
+++ org.cse.visiri.core/src/main/java/org/cse/visiri/core/Agent.java
@@ -1,10 +1,15 @@
 package org.cse.visiri.core;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.cse.visiri.algo.util.UtilizationUpdater;
 import org.cse.visiri.communication.Environment;
 import org.cse.visiri.engine.EngineHandler;
 import org.cse.visiri.util.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 
 /**
@@ -17,57 +22,53 @@ public class Agent extends Thread {
     private EngineHandler engineHandler;
     private Queue<Double> utilizationLevelQueue;
     private WindowQueue windowQueue;
-    private double memCount;
-    private int count;
 
-    public Agent(EngineHandler engineHandler,UtilizationUpdater utilizationUpdater){
+    public Agent(EngineHandler engineHandler){
         environment = Environment.getInstance();
         this.engineHandler=engineHandler;
-        //this.utilizationUpdater=new UtilizationUpdater();
-        this.utilizationUpdater=utilizationUpdater;
+        this.utilizationUpdater=new UtilizationUpdater();
         this.utilizationUpdater.start();
         utilizationLevelQueue=new ArrayDeque<Double>();
         windowQueue=new WindowQueue(6);
-        this.memCount=0;
-        this.count=0;
+    }
+
+    private void checkEventRate(){
+        if(Environment.getInstance().checkDynamic2()) {
+            Map<String, Double> rates = Environment.getInstance().getNodeEventRates();
+            if (rates.get(Environment.getInstance().getNodeId()) > Configuration.MAX_EVENT_RATE) {
+               if(Environment.getInstance().checkDynamic2()) {
+                  Environment.getInstance().disableDynamic2();
+                  transferEngines();
+               }
+            }
+        }
+    }
+
+    private void checkManualDynamic(){
+        if(Environment.getInstance().checkDynamic()){
+            Environment.getInstance().disableDynamic();
+            System.out.println("Dynamic adjustments started . . . ");
+            transferEngines();
+        }
     }
 
     public void run(){
         System.out.println("Agent started . . .");
         while(true){
-
-
             if(!Environment.getInstance().checkTransferInprogress()){
-
-
-//                if(Environment.getInstance().checkDynamic()){
-//                        System.out.println("Dynamic adjustments started . . . ");
-//                        transferEngines();
-//                        Environment.getInstance().disableDynamic();
-//                      // break;
-//                }else{
-//                   // System.out.println("check . . .");
-//                }
-
+//                checkManualDynamic();
+                checkEventRate();
+                /*
                     Utilization utilization=utilizationUpdater.update();
 
                     double utilizationLevel=utilization.getOverallUtilizationValue();
-                    memCount+=utilizationLevel;
-                    count++;
                     windowQueue.add(utilizationLevel);
                     double utilizationLevelAvg=windowQueue.getAverage();
 
-                System.out.println("**********************");
-                System.out.println("JVM usage : " +utilization.getJVMCpuUtilization());
-                System.out.println("Average CPU load : "+ utilization.getAverageSystemLoad());
-                System.out.println("Free memory persentage : "+utilization.getFreeMemoryPercentage());
-                System.out.println("Recent CPU usage : "+utilization.getRecentCpuUsage());
-                System.out.println("***********************");
-
                     if(utilizationLevelAvg>=Configuration.UTILIZATION_THRESHOULD){
-//                       transferEngines();
+                       transferEngines();
                     }
-
+*/
             }
             try {
                 sleep(Configuration.AGENT_UPDATE_PERIOD);
@@ -82,9 +83,7 @@ public class Agent extends Thread {
             TransferbleEngine TREngines=engineHandler.getTransferableEngines();
             Map<String,List<Query>> transferableEngines=TREngines.getNodeTransferbleQueryMap();
             Set<StreamDefinition> removableEngines=TREngines.getCompletlyRemovedEvents();
-
             Environment.getInstance().addRemovablesToDispatcher(removableEngines);
-
             System.out.println("\nSelected nodes : "+transferableEngines.size());
 
             for(String ip : transferableEngines.keySet()){
@@ -93,23 +92,12 @@ public class Agent extends Thread {
 
                 for(Query query : queries){
                     System.out.println(query.getQueryId());
-
-//                    for(StreamDefinition streamDefinition : query.getInputStreamDefinitionsList()){
-//                        environment.getInstance().getBufferingEventList().add(streamDefinition.getStreamId());
-//                        System.out.println("Buffering list "+streamDefinition.getStreamId());
-//                    }
-
                 }
-
             }
 
             Environment.getInstance().createNewTransferable(transferableEngines);
             Environment.getInstance().sendEvent(Environment.EVENT_TYPE_ENGINE_PASS);
             System.out.println("\nENGINE_PASS Message sent");
-    }
-
-    public double getOverallAverageMemory(){
-        return memCount/count;
     }
 
 }
