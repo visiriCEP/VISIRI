6fb0a2cdda0be544b94128d95c111c402959d866
ujitha <dgujitha@gmail.com>
2015-02-19 17:56:42 +0530

    add Apache licence to Environment

--- org.cse.visiri.communication.environment/src/main/java/org/cse/visiri/communication/Environment.java
+++ org.cse.visiri.communication.environment/src/main/java/org/cse/visiri/communication/Environment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.cse.visiri.communication;
 
 import com.hazelcast.config.Config;
@@ -10,9 +26,7 @@ import java.io.File;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by Geeth on 2014-10-31.
- */
+
 public class Environment implements MessageListener {
 
     public static final int NODE_TYPE_PROCESSINGNODE = 1;
